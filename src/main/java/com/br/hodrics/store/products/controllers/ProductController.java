19d85488e88361315e9b71701005260037744ca8
Rodrigo Nascimento <rodrigo_allexandre@yahoo.com.br>
2026-01-01 19:08:24 -0300

    add integration s3

--- src/main/java/com/br/hodrics/store/products/controllers/ProductController.java
+++ src/main/java/com/br/hodrics/store/products/controllers/ProductController.java
@@ -1,16 +1,18 @@
 package com.br.hodrics.store.products.controllers;
 
-import com.br.hodrics.deps.dtos.products.ProductDto;
+import com.br.hodrics.deps.dtos.products.ProductDTO;
 import com.br.hodrics.store.products.services.MsProductService;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.multipart.MultipartFile;
 
+import java.math.BigDecimal;
 import java.util.List;
 
 @RestController
-@RequestMapping("/products")
+@RequestMapping("/store/products")
 public class ProductController {
     private final MsProductService service;
 
@@ -18,9 +20,36 @@ public class ProductController {
         this.service = service;
     }
 
-    @GetMapping("/getall")
-    public ResponseEntity<List<ProductDto>> findAll(){
+    @GetMapping
+    public ResponseEntity<List<ProductDTO>> findAll(){
         return ResponseEntity.ok().body(service.listProducts());
     }
 
+    @GetMapping("/{id}")
+    public ResponseEntity<ProductDTO> getById(@PathVariable String id){
+        return ResponseEntity.ok().body(service.findById(id));
+    }
+
+    @PostMapping(consumes = MediaType.MULTIPART_FORM_DATA_VALUE)
+    public ResponseEntity<Void> save(
+                                      @RequestPart("title") String title,
+                                      @RequestPart("price") String price,
+                                      @RequestPart("description") String description,
+                                      @RequestPart("category") String category,
+                                      @RequestPart("image") MultipartFile image) {
+        service.create(title, price, description, category, image);
+        return ResponseEntity.status(HttpStatus.CREATED).build();
+    }
+
+    @PutMapping(value = "{id}")
+    public ResponseEntity<Void> update(@RequestBody ProductDTO productDto,@PathVariable String id) {
+        service.update(id, productDto);
+        return ResponseEntity.ok().build();
+    }
+
+    @DeleteMapping(value = "{id}")
+    public ResponseEntity<Void> delete(@PathVariable String id) {
+        service.delete(id);
+        return ResponseEntity.ok().build();
+    }
 }
--- src/main/java/com/br/hodrics/store/products/services/MsProductService.java
+++ src/main/java/com/br/hodrics/store/products/services/MsProductService.java
@@ -1,21 +1,62 @@
 package com.br.hodrics.store.products.services;
 
-import com.br.hodrics.deps.dtos.products.ProductDto;
-import com.br.hodrics.deps.services.ProductService;
+import com.br.hodrics.deps.dtos.products.ProductDTO;
+import com.br.hodrics.deps.exceptions.IntegrationException;
+import com.br.hodrics.deps.services.aws.S3UploadService;
+import com.br.hodrics.deps.services.aws.dynamo.ProductDynamoService;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Service;
+import org.springframework.web.multipart.MultipartFile;
 
+import java.math.BigDecimal;
 import java.util.List;
 
 @Service
 public class MsProductService {
 
-    private final ProductService productService;
+    private static final Logger log = LoggerFactory.getLogger(MsProductService.class);
+    private final S3UploadService s3UploadService;
+    private final ProductDynamoService productService;
 
-    public MsProductService(ProductService productService) {
+    public MsProductService(S3UploadService s3UploadService, ProductDynamoService productService) {
+        this.s3UploadService = s3UploadService;
         this.productService = productService;
     }
 
-    public List<ProductDto> listProducts(){
-        return productService.listProducts();
+    public List<ProductDTO> listProducts(){
+        return productService.getAll();
+    }
+
+    public ProductDTO findById(String id){
+        return productService.getById(id);
+    }
+
+    public void delete(String id){
+        productService.delete(id);
+    }
+
+    public void update(String id, ProductDTO productDto){
+        productService.update(id, productDto);
+    }
+
+    public void create(String title, String price, String description, String category, MultipartFile image) {
+        log.info("MsProductService create product start");
+        try {
+           final ProductDTO productDto = new ProductDTO(title,new BigDecimal(price),description,category);
+
+           final String idProduct =  productService.create(productDto);
+           final String keyImageInBucket = this.uploadImage(image, idProduct);
+           productDto.setImage(keyImageInBucket);
+           this.productService.update(idProduct, productDto);
+
+        }catch (Exception e){
+            log.error("MsProductService error when create new product {}" , title, e);
+            throw new IntegrationException("MsProductService error when create new product");
+        }
+    }
+
+    private String uploadImage(MultipartFile file, String key){
+        return  s3UploadService.uploadImage(file, key);
     }
 }
